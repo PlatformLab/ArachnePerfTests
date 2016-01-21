f26781a2d170a8a859b67a6fe75b55a869c46520
Henry Qin <hq6@stanford.edu>
2016-01-20 17:39:53 -0800

    This commit represents a segfault on the instruction that we do not understand

--- ArachneTest.cc
+++ ArachneTest.cc
@@ -3,24 +3,18 @@
 
 #include "Arachne.h"
 #include "Cycles.h"
+#include "TimeTrace.h"
 
 
 using PerfUtils::Cycles;
 
 void printEveryTwo(int start, int end) {
-    std::vector<uint64_t> cycleCounts;
-    cycleCounts.reserve(100000);
     for (int i = start; i < end; i+=2) {
-       cycleCounts.push_back(Cycles::rdtsc());
-//       printf("%d is yielding\n", i);
+       PerfUtils::TimeTrace::getGlobalInstance()->record("User Thread is yielding...");
        Arachne::yield();
     }
     if (start == 2) {
-      uint64_t startTime = Cycles::toNanoseconds(cycleCounts[0]);
-      for (unsigned int i = 0; i < cycleCounts.size(); i++) {
-            printf("%lu\n", Cycles::toNanoseconds(cycleCounts[i]) - startTime);
-        }
-        fflush(stdout);
+        PerfUtils::TimeTrace::getGlobalInstance()->print();
     }
 }
 
