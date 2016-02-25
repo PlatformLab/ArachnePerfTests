c14c369165ac39bcf302bf034bf6b32037da2ad6
Henry Qin <hq6@stanford.edu>
2016-02-25 01:51:54 -0800

    Migrated the main code into an ArachneThread

--- ArachneTest.cc
+++ ArachneTest.cc
@@ -13,22 +13,30 @@ using PerfUtils::TimeTrace;
 void printEveryTwo(int start, int end) {
 }
 
-int main(){
-    // Initialize the library
-    Arachne::threadInit();
+int realMain() {
     uint64_t startTime = Cycles::rdtsc();
 
     // Measure the thread creation overhead in the creating thread.
-    for (int i = 0; i < NUM_THREADS; i++)
-        Arachne::createThread(-1, printEveryTwo,1,i);
-//        Arachne::createThread([=]() {printEveryTwo(1,i);});
-    
-    uint64_t timePerYield = (Cycles::rdtsc() - startTime) / NUM_THREADS;
+    for (int i = 0; i < NUM_THREADS - 1; i++) {
+        int id = Arachne::createThread(-1, printEveryTwo,1,i);
+        if (id) {
+            fprintf(stderr, "Thread Creation Error on i = %d\n", i);
+            exit(-1);
+        }
+    }
+
+    uint64_t timePerYield = (Cycles::rdtsc() - startTime) /(NUM_THREADS - 1);
     printf("Thread creation average time %lu\n", Cycles::toNanoseconds(timePerYield));
-
     TimeTrace::getGlobalInstance()->print();
-
     fflush(stdout);
+    return 0;
+}
+
+int main(){
+    // Initialize the library
+    Arachne::threadInit();
+    Arachne::createThread(-1, realMain);
     // Must be the last call
     Arachne::mainThreadJoinPool();
+    return 0;
 }
