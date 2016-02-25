d9b6f57b27f2722fd9ff69847f8b70c286666f1f
Henry Qin <hq6@stanford.edu>
2016-02-24 21:48:20 -0800

    Adding a comparison test for std::thread

--- Makefile
+++ Makefile
@@ -1,10 +1,15 @@
 
 DEBUG=-g
 LIBS=-I../Arachne  -L../Arachne -lArachne  -I../PerfUtils -L../PerfUtils -lPerfUtils  -pthread
+CXXFLAGS=-std=c++11 -O2
 
+all: ArachneTest ThreadCreationTest
 ArachneTest: ArachneTest.cc ../Arachne/libArachne.a
-	g++ -Wall -Werror -O2 $(DEBUG) -std=c++11  ArachneTest.cc $(LIBS) -o ArachneTest
+	g++ -Wall -Werror $(DEBUG) $(CXXFLAGS)  ArachneTest.cc $(LIBS) -o ArachneTest
+
+ThreadCreationTest: ThreadCreationTest.cc
+	g++ -Wall -Werror $(DEBUG) $(CXXFLAGS)  ThreadCreationTest.cc $(LIBS) -o ThreadCreationTest
 
 clean:
-	rm -f ArachneTest
+	rm -f ArachneTest ThreadCreationTest
 
--- ThreadCreationTest.cc
+++ ThreadCreationTest.cc
@@ -1,30 +1,24 @@
 #include <stdio.h>
 #include <vector>
+#include <thread>
 
-#include "Arachne.h"
 #include "Cycles.h"
-#include "TimeTrace.h"
 
 
 using PerfUtils::Cycles;
-using PerfUtils::TimeTrace;
 
 void printEveryTwo(int start, int end) {
 }
 
 int main(){
-    // Initialize the library
-    Arachne::threadInit();
     uint64_t startTime = Cycles::rdtsc();
 
     // Measure the thread creation overhead in the creating thread.
     for (int i = 0; i < 1000; i++)
-        Arachne::createThread(-1, printEveryTwo,1,i);
+        std::thread(printEveryTwo,1,i).detach();
     
     uint64_t timePerYield = (Cycles::rdtsc() - startTime) / 1000;
     printf("Thread creation average time %lu\n", Cycles::toNanoseconds(timePerYield));
 
     fflush(stdout);
-    // Must be the last call
-    Arachne::mainThreadJoinPool();
 }
