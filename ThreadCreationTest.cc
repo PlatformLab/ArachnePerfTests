bae55b54ba152be7741f93ec215214a8774c06af
Henry Qin <hq6@stanford.edu>
2016-04-07 18:40:33 -0700

    Benchmark thread creation using std::thread

--- ThreadCreationTest.cc
+++ ThreadCreationTest.cc
@@ -1,24 +1,38 @@
 #include <stdio.h>
 #include <vector>
 #include <thread>
+#include <unistd.h>
+#include <sched.h>
+#include <pthread.h>
 
 #include "Cycles.h"
+#include "TimeTrace.h"
 
 
 using PerfUtils::Cycles;
+using PerfUtils::TimeTrace;
 
 void printEveryTwo(int start, int end) {
+    TimeTrace::getGlobalInstance()->record("Inside thread");
+//    printf("Our scheduler: %d, SCHED_RR: %d\n", sched_getscheduler(0), SCHED_RR);
+//    fflush(stdout);
 }
 
 int main(){
-    uint64_t startTime = Cycles::rdtsc();
+    struct sched_param param;
+    param.sched_priority = 99;
+    sched_setscheduler(0, SCHED_RR, &param);
 
     // Measure the thread creation overhead in the creating thread.
-    for (int i = 0; i < 1000; i++)
-        std::thread(printEveryTwo,1,i).detach();
-    
-    uint64_t timePerYield = (Cycles::rdtsc() - startTime) / 1000;
-    printf("Thread creation average time %lu\n", Cycles::toNanoseconds(timePerYield));
+    for (int i = 0; i < 10000; i++) {
+        TimeTrace::getGlobalInstance()->record("A thread is about to be born!");
+        std::thread t(printEveryTwo,1,i);
+//        pthread_setschedparam(t.native_handle(), SCHED_RR, &param);
+        t.detach();
+        usleep(100);
+//        TimeTrace::getGlobalInstance()->record("Finished usleep 20!");
+    }
 
+    TimeTrace::getGlobalInstance()->print();
     fflush(stdout);
 }
