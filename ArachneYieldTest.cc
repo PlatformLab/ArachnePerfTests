c45b398a3fe60348068b92b8198a843a05924156
Henry Qin <hq6@stanford.edu>
2016-04-08 18:40:21 -0700

    Arachne portion of yield experiment.

---
+++ ArachneYieldTest.cc
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <vector>
+
+#include "Arachne.h"
+#include "Cycles.h"
+#include "TimeTrace.h"
+
+
+using PerfUtils::Cycles;
+using PerfUtils::TimeTrace;
+
+void printEveryTwo(int start, int end) {
+    uint64_t startTime = Cycles::rdtsc();
+    for (int i = start; i < end; i+=2) {
+//        printf("Yielding after %d\n", i);
+        Arachne::yield();
+    }
+    if (start == 1) {
+        uint64_t timePerYield = (Cycles::rdtsc() - startTime) / (end - start);
+        printf("%lu\n", Cycles::toNanoseconds(timePerYield));
+    }
+}
+
+int main(){
+    // Initialize the library
+    Arachne::threadInit();
+
+    // Add some work
+    Arachne::createThread(1, printEveryTwo, 1, 9999);
+    Arachne::createThread(1, printEveryTwo, 2, 10000);
+    
+    fflush(stdout);
+    // Must be the last call
+    Arachne::mainThreadJoinPool();
+}
--- Makefile
+++ Makefile
@@ -3,9 +3,12 @@ DEBUG=-g
 LIBS=-I../Arachne  -L../Arachne -lArachne  -I../PerfUtils -L../PerfUtils -lPerfUtils  -pthread
 CXXFLAGS=-std=c++11 -O2
 
-all: ArachneTest ThreadCreationTest GoThreadCreate
+all: ArachneTest ThreadCreationTest GoThreadCreate ArachneYieldTest
 ArachneTest: ArachneTest.cc ../Arachne/libArachne.a
 	g++ -Wall -Werror $(DEBUG) $(CXXFLAGS)  ArachneTest.cc $(LIBS) -o ArachneTest
+
+ArachneYieldTest: ArachneYieldTest.cc ../Arachne/libArachne.a
+	g++ -Wall -Werror $(DEBUG) $(CXXFLAGS)  ArachneYieldTest.cc $(LIBS) -o ArachneYieldTest
 
 ThreadCreationTest: ThreadCreationTest.cc
 	g++ -Wall -Werror $(DEBUG) $(CXXFLAGS)  ThreadCreationTest.cc $(LIBS) -o ThreadCreationTest
