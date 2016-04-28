7830fa06b1b782cde5b685ea1b80d291da568b1a
Henry Qin <hq6@stanford.edu>
2016-04-27 23:32:41 -0700

    Added numThread argument to ThreadYield test

--- ThreadYieldTest.cc
+++ ThreadYieldTest.cc
@@ -20,22 +20,18 @@ int core = 0;
 void printEveryN(int start, int end, int increment) {
     pinThreadToCore(core);
     uint64_t startTime = Cycles::rdtsc();
-    for (int i = start; i < end; i+=2) {
-//        TimeTrace::getGlobalInstance()->record("Thread %d is yielding", start);
+    for (int i = start; i < end; i+=increment) {
         std::this_thread::yield();
     }
 
-    if (start == 2) {
-        uint64_t timePerYield = (Cycles::rdtsc() - startTime) / (end - start);
-        printf("%lu\n", Cycles::toNanoseconds(timePerYield));
-//        TimeTrace::getGlobalInstance()->print();
-    }
+    uint64_t timePerYield = (Cycles::rdtsc() - startTime) / (end - start);
+    printf("%lu\n", Cycles::toNanoseconds(timePerYield));
 }
 
 /**
   * This program measures the cost of a context switch on the same core for two kernel threads.
   */
-int main(){
+int main(int argc, char** argv) {
     struct sched_param param;
     param.sched_priority = 99;
     int err = sched_setscheduler(0, SCHED_RR, &param);
@@ -45,8 +41,15 @@ int main(){
     }
     core = sched_getcpu();
 
-    std::thread(printEveryN,1,9999,2).detach();
-    std::thread(printEveryN,2,10000,2).detach();
+    if (argc < 2) {
+        std::thread(printEveryN,1,9999,2).detach();
+        std::thread(printEveryN,2,10000,2).detach();
+    }
+    else {
+        int numThreads = atoi(argv[1]);
+        for (int i = 1; i < numThreads+1; i++)
+            std::thread(printEveryN,i,i+9998,numThreads).detach();
+    }
 
     fflush(stdout);
     while (true);
