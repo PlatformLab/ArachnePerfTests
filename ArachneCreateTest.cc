98f6159cad9c96d38d4ff9d3b6779db5d92be03f
Henry Qin <hq6@stanford.edu>
2016-06-21 17:25:25 -0700

    Measuring with no sleeps, preparing for divide-by-n test

--- ArachneCreateTest.cc
+++ ArachneCreateTest.cc
@@ -13,10 +13,10 @@ using PerfUtils::Cycles;
 using PerfUtils::TimeTrace;
 
 void ObjectTask(void *objectPointer) {
-    flag = 1;
     PerfUtils::TimeTrace::getGlobalInstance()->record("Inside thread");
     objectPointer = (char*)objectPointer+1;
     PerfUtils::TimeTrace::getGlobalInstance()->record("Incremented pointer that was passed to this thread");
+    flag = 1;
 }
 
 
@@ -24,15 +24,10 @@ int realMain() {
     // Cross-core creation
     void *dummy = (void*) 0x0;
     for (int i = 0; i < NUM_THREADS; i++) {
-        PerfUtils::TimeTrace::getGlobalInstance()->record("DummyTrace!");
-        Arachne::sleep(20);
-        PerfUtils::TimeTrace::getGlobalInstance()->record("DummyTrace2!");
         PerfUtils::TimeTrace::getGlobalInstance()->record("A thread is about to be born!");
         flag = 0;
         Arachne::createThread(1, ObjectTask, dummy);
         while (!flag);
-        // Delay a full microsecond before creating the next thread
-        Arachne::sleep(1000);
     }
 
     TimeTrace::getGlobalInstance()->print();
