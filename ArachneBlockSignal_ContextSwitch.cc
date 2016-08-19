fb723aaafaa99076da519322abec0a29e1d299f3
Henry Qin <hq6@stanford.edu>
2016-08-19 00:34:42 -0700

    ArachneBlockSignal_ContextSwitch now works correctly with two threads

--- ArachneBlockSignal_ContextSwitch.cc
+++ ArachneBlockSignal_ContextSwitch.cc
@@ -16,22 +16,27 @@ using PerfUtils::TimeTrace;
 
 #define NUM_ITERATIONS 10000
 
-volatile int flag;
-
 // Used for filling up the run queue
 volatile int creationFlag;
 
 
-void producer(Arachne::ThreadId tid1, Arachne::ThreadId tid2) {
+void producer(Arachne::ThreadId *tids, int numTids, volatile int *flags) {
     printf("producerId = %p\n", Arachne::getThreadId());
-    Arachne::signal(tid1); 
-	for (int i = 0; i < NUM_ITERATIONS; i++) {
-		while (!flag);
-		flag = 0;
-        TimeTrace::record("Producer about to signal");
-        Arachne::signal(tid1); 
-        TimeTrace::record("Producer finished signaling");
-        Arachne::yield();
+    Arachne::signal(tids[0]); 
+    Arachne::signal(tids[1]); 
+
+    printf("flags in producer = %p\n", flags);
+
+	for (int i = 0; i < NUM_ITERATIONS*numTids; i++) {
+        int index = i % numTids;
+		while (!flags[index]);
+		flags[index] = 0;
+        int nextIndex = (i+1) % numTids;
+//        printf("Producer about to signal %d\n", nextIndex);
+        TimeTrace::record("Producer about to signal %x", nextIndex);
+        Arachne::signal(tids[nextIndex]); 
+        TimeTrace::record("Producer finished signaling %x", nextIndex);
+        Arachne::sleep(200);
 	}
     printf("Producer finished\n");
     fflush(stdout);
@@ -39,21 +44,23 @@ void producer(Arachne::ThreadId tid1, Arachne::ThreadId tid2) {
     TimeTrace::print();
 }
 
-void consumer(Arachne::ThreadId& tid) {
+void consumer(Arachne::ThreadId& tid, int cid, volatile int* flag) {
     tid = Arachne::getThreadId();
      __asm__ __volatile__("sfence" ::: "memory");
     creationFlag = 0;
     printf("&tid = %p\n", &tid);
+    printf("flag in consumer = %p\n", flag);
     Arachne::block();
-    printf("ConsumerId = %p\n", tid);
-    fflush(stdout);
+    Arachne::yield();
+    printf("cid = %d, ConsumerId = %p\n", cid, tid);
 	for (int i = 0; i < NUM_ITERATIONS; i++) {
-		while (flag);
-		flag = 1;
+		while (*flag);
+		*flag = 1;
         Arachne::block();
-        TimeTrace::record("Consumer just woke up");
+        TimeTrace::record("Consumer just woke up %x", cid);
+//        printf("ConsumerId = %d, i = %d\n", cid, i);
 	}
-    printf("Consumer finished\n");
+    printf("Consumer %d finished\n", cid);
     fflush(stdout);
 }
 
@@ -86,19 +93,18 @@ int main(int argc, char** argv){
     // meant to pass.
     // DONE: It appears that my threading library has the same quirk with reference parameters that std::thread has, and also the same solution.
     // http://stackoverflow.com/questions/21048906/stdthread-pass-by-reference-calls-copy-constructor
-    Arachne::ThreadId tid1;
-    Arachne::ThreadId tid2;
-    printf("&tid1 = %p, &tid2 = %p\n", &tid1, &tid2);
+    Arachne::ThreadId tids[2];
+    volatile int flags[2] = {0}; // Initialized to 0, let consumers go first.
+    printf("flags in the original stack = %p\n", flags);
 
     creationFlag = 1;     
-	Arachne::createThread(1, consumer, std::ref(tid1));
+	Arachne::createThread(1, consumer, std::ref(tids[0]), 0, flags);
     while (creationFlag);
     creationFlag = 1;     
-	Arachne::createThread(1, consumer, std::ref(tid2));
+	Arachne::createThread(1, consumer, std::ref(tids[1]), 1, flags + 1);
     while (creationFlag);
-    printf("%p %p\n", tid1, tid2);
     sleep(1);
-	Arachne::createThread(0, producer, tid1, tid2);
+	Arachne::createThread(0, producer, tids, 2, flags);
 
     printf("Created Producer and consumer threads\n");
     fflush(stdout);
