fb075e134a131fd32038327227566d656ec929ac
Henry Qin <hq6@stanford.edu>
2016-08-19 14:14:24 -0700

    Simplified parameter passing and removed debugging statements

--- ArachneBlockSignal_ContextSwitch.cc
+++ ArachneBlockSignal_ContextSwitch.cc
@@ -15,26 +15,22 @@ using PerfUtils::TimeTrace;
 
 
 #define NUM_ITERATIONS 10000
-#define NUM_THREADS_IN_CYCLE 5
+#define NUM_THREADS_IN_CYCLE 1
 
 // Used for filling up the run queue
 volatile int creationFlag;
 
+Arachne::ThreadId tids[NUM_THREADS_IN_CYCLE];
+// Initialized to 0, let consumers go first.
+volatile int flags[NUM_THREADS_IN_CYCLE] = {0};
 
-void producer(Arachne::ThreadId *tids, int numTids, volatile int *flags) {
-    printf("producerId = %p\n", Arachne::getThreadId());
-    for (int i = 0; i < numTids; i++)
-        Arachne::signal(tids[i]); 
-
-    printf("flags in producer = %p\n", flags);
-
-	for (int i = 0; i < NUM_ITERATIONS*numTids; i++) {
-        int index = i % numTids;
+void producer() {
+	for (int i = 0; i < NUM_ITERATIONS*NUM_THREADS_IN_CYCLE; i++) {
+        int index = i % NUM_THREADS_IN_CYCLE;
+        int nextIndex = index + 1;
+        if (nextIndex == NUM_THREADS_IN_CYCLE) nextIndex = 0;
 		while (!flags[index]);
 		flags[index] = 0;
-        int nextIndex = index + 1;
-        if (nextIndex == numTids) nextIndex = 0;
-//        printf("Producer about to signal %d\n", nextIndex);
         TimeTrace::record("Producer about to signal %x", nextIndex);
         Arachne::signal(tids[nextIndex]); 
         TimeTrace::record("Producer finished signaling %x", nextIndex);
@@ -46,21 +42,15 @@ void producer(Arachne::ThreadId *tids, int numTids, volatile int *flags) {
     TimeTrace::print();
 }
 
-void consumer(Arachne::ThreadId& tid, int cid, volatile int* flag) {
-    tid = Arachne::getThreadId();
+void consumer(int cid) {
+    tids[cid] = Arachne::getThreadId();
      __asm__ __volatile__("sfence" ::: "memory");
     creationFlag = 0;
-    printf("&tid = %p\n", &tid);
-    printf("flag in consumer = %p\n", flag);
-    Arachne::block();
-    Arachne::yield();
-    printf("cid = %d, ConsumerId = %p\n", cid, tid);
 	for (int i = 0; i < NUM_ITERATIONS; i++) {
-		while (*flag);
-		*flag = 1;
+		while (flags[cid]);
+		flags[cid] = 1;
         Arachne::block();
         TimeTrace::record("Consumer just woke up %x", cid);
-//        printf("ConsumerId = %d, i = %d\n", cid, i);
 	}
     printf("Consumer %d finished\n", cid);
     fflush(stdout);
@@ -84,22 +74,12 @@ int main(int argc, char** argv){
         while (creationFlag);
     }
 
-    Arachne::ThreadId tids[NUM_THREADS_IN_CYCLE];
-
-    // Initialized to 0, let consumers go first.
-    volatile int flags[NUM_THREADS_IN_CYCLE] = {0}; 
-    printf("flags in the original stack = %p\n", flags);
-
     for (int i = 0; i < NUM_THREADS_IN_CYCLE; i++) {
         creationFlag = 1;     
-        // It appears that my threading library has the same quirk with reference
-        // parameters that std::thread has, and also the same solution: std::ref.
-        // http://stackoverflow.com/questions/21048906/stdthread-pass-by-reference-calls-copy-constructor
-        Arachne::createThread(1, consumer, std::ref(tids[i]), i, flags + i);
+        Arachne::createThread(1, consumer,  i);
         while (creationFlag);
     }
-    sleep(1);
-	Arachne::createThread(0, producer, tids, NUM_THREADS_IN_CYCLE, flags);
+	Arachne::createThread(0, producer);
 
     printf("Created Producer and consumer threads\n");
     fflush(stdout);
