db76b11df70621458709972b1c988d0f1efc0f39
Henry Qin <hq6@stanford.edu>
2016-08-31 21:54:16 -0700

    Adding colors to make it easier to distinguish tests

--- RunAllTests.sh
+++ RunAllTests.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+echo -e $GREEN"Yield Test"$NC
 make ArachneYieldTest && timeout 3 ./ArachneYieldTest
+echo -e "\n\n"
+
 ./RunTest.sh CV "Producer about"
 ./RunTest.sh BlockSignal_ContextSwitch "Producer about to signal 0"
 ./RunTest.sh BlockSignal "Producer about to signal"
--- RunTest.sh
+++ RunTest.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+echo -e "${GREEN}${1}Test${NC}"
 make Arachne${1}Test  && rm -f ${1}.log && ( timeout  3 ./Arachne${1}Test  ) ; ttsum.py -f "$2" ${1}.log
+echo -e "\n\n"
