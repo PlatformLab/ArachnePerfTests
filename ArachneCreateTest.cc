5bb0387b2ab32d0ea9e9f29a6e98517dd1a7e66a
Henry Qin <hq6@stanford.edu>
2017-04-06 23:23:35 -0700

    Updated run script and updated creation to use join

--- ArachneCreateTest.cc
+++ ArachneCreateTest.cc
@@ -9,8 +9,6 @@
 
 #define NUM_SAMPLES 1000000
 
-volatile int flag;
-
 using PerfUtils::Cycles;
 
 std::atomic<uint64_t> arrayIndex;
@@ -19,7 +17,6 @@ uint64_t latencies[NUM_SAMPLES];
 void task(uint64_t creationTime) {
     uint64_t latency = Cycles::rdtsc() - creationTime;
     latencies[arrayIndex++] = latency;
-    flag = 1;
 }
 
 
@@ -29,15 +26,14 @@ int realMain() {
     // Do some extra work before starting the next thread.
     uint64_t k = 0;
     for (int i = 0; i < NUM_SAMPLES; i++) {
-        flag = 0;
-        Arachne::createThreadOnCore(1, task, Cycles::rdtsc());
-        while (!flag) Arachne::yield();
+        Arachne::ThreadId id = Arachne::createThreadOnCore(1, task, Cycles::rdtsc());
+        Arachne::join(id);
         for (uint64_t j = 0; j < 10000U; j++) k += j;
     }
     FILE* devNull = fopen("/dev/null", "w");
     fprintf(devNull,"%lu\n", k);
     fclose(devNull);
-    while (!flag) Arachne::yield();
+
     Arachne::shutDown();
     return 0;
 }
--- RunAllTests.sh
+++ RunAllTests.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-GREEN='\033[0;32m'
-NC='\033[0m'
-
-./ArachneCVTest
-./ArachneBlockSignalTest
 ./ArachneCreateTest
 ./ArachneYieldTest
+./ArachneCVTest
+./ArachneBlockSignalTest
