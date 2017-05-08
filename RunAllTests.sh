d3fd26491e4e24d7f062dbcc01139c104ae7f7ea
Henry Qin <hq6@stanford.edu>
2017-05-07 20:31:46 -0700

    Fixed RunAlltests to print only a single header

--- RunAllTests.sh
+++ RunAllTests.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-./ArachneCreateTest
-./NullYieldTest
-./ArachneYieldTest
-./ArachneCVTest
-./ArachneBlockSignalTest
+echo Benchmark,Count,Avg,Median,Min,99%,99.9%,99.99%,Max
+./ArachneCreateTest | tail -n 1
+./NullYieldTest | tail -n 1
+./ArachneYieldTest | tail -n 1
+./ArachneCVTest | tail -n 1
+./ArachneBlockSignalTest | tail -n 1
