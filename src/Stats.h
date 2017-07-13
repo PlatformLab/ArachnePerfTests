782550bd660a1703975ad055fe95252148afb479
Henry Qin <hq6@stanford.edu>
2017-07-13 11:23:56 -0700

    Updating stats to compute more percentiles.

--- src/Stats.h
+++ src/Stats.h
@@ -1,30 +1,71 @@
 #include <math.h>
 #include "mkdir_p.h"
 
+struct Statistics {
+    size_t count;
+    uint64_t average;
+    uint64_t min;
+    uint64_t median;
+    uint64_t P10;
+    uint64_t P20;
+    uint64_t P30;
+    uint64_t P40;
+    uint64_t P50;
+    uint64_t P60;
+    uint64_t P70;
+    uint64_t P80;
+    uint64_t P90;
+    uint64_t P99;
+    uint64_t P999;
+    uint64_t P9999;
+    uint64_t max;
+};
+
 int compare(const void * a, const void * b)
 {
     if (*(const uint64_t*)a == *(const uint64_t*)b) return 0;
     return  *(const uint64_t*)a < *(const uint64_t*)b ? -1 : 1;
 }
 
-void printStatistics(const char* label, uint64_t* rawdata, size_t count,
-        const char* datadir) {
-    static bool headerPrinted = false;
+Statistics computeStatistics(uint64_t* rawdata, size_t count) {
+    Statistics stats;
+    if (count == 0)
+        return stats;
     qsort(rawdata, count, sizeof(uint64_t), compare);
     uint64_t sum = 0;
     for (size_t i = 0; i < count; i++)
         sum += rawdata[i];
-    uint64_t avg = sum / count;
+    stats.average  = sum / count;
+    stats.min    = rawdata[0];
+    stats.median = rawdata[count / 2];
+    stats.P10    = rawdata[(int)(static_cast<double>(count)*0.1)];
+    stats.P20    = rawdata[(int)(static_cast<double>(count)*0.2)];
+    stats.P30    = rawdata[(int)(static_cast<double>(count)*0.3)];
+    stats.P40    = rawdata[(int)(static_cast<double>(count)*0.4)];
+    stats.P50    = rawdata[(int)(static_cast<double>(count)*0.5)];
+    stats.P60    = rawdata[(int)(static_cast<double>(count)*0.6)];
+    stats.P70    = rawdata[(int)(static_cast<double>(count)*0.7)];
+    stats.P80    = rawdata[(int)(static_cast<double>(count)*0.8)];
+    stats.P90    = rawdata[(int)(static_cast<double>(count)*0.9)];
+    stats.P99    = rawdata[(int)(static_cast<double>(count)*0.99)];
+    stats.P999   = rawdata[(int)(static_cast<double>(count)*0.999)];
+    stats.P9999  = rawdata[(int)(static_cast<double>(count)*0.9999)];
+    stats.max    = rawdata[count-1];
+    return stats;
+}
 
+void printStatistics(const char* label, uint64_t* rawdata, size_t count,
+        const char* datadir = NULL) {
+    static bool headerPrinted = false;
     if (!headerPrinted) {
         puts("Benchmark,Count,Avg,Median,Min,99%,"
                 "99.9%,99.99%,Max");
         headerPrinted = true;
     }
-    printf("%s,%zu,%lu,%lu,%lu,%lu,%lu,%lu,%lu\n", label, count, avg,
-            rawdata[count / 2], rawdata[0], rawdata[(int)(static_cast<double>(count)*0.99)],
-            rawdata[(int)(static_cast<double>(count)*0.999)],rawdata[(int)(static_cast<double>(count)*0.9999)],
-            rawdata[count-1]);
+    Statistics stats = computeStatistics(rawdata, count);
+    printf("%s,%zu,%lu,%lu,%lu,%lu,%lu,%lu,%lu\n", label, count, stats.average,
+            stats.median, stats.min, stats.P99,
+            stats.P999, stats.P9999, stats.max);
 
     // Dump the data out
     if (datadir != NULL) {
