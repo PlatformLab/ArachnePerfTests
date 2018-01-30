5e380eb163710814b3b35a09592f251b6b5f1cf5
Henry Qin <hq6@stanford.edu>
2018-01-30 11:37:19 -0800

    Suppress stderr in RunAllTests.sh
    An earlier commit made all of the Core Arbiter's client-side log messages go to stderr.
    This commit suppresses those extra logs.

--- scripts/RunAllTests.sh
+++ scripts/RunAllTests.sh
@@ -12,7 +12,7 @@ bin/NullYieldTest | tail -n 1 ;
 bin/ArachneYieldTest | tail -n 1 ;
 bin/ArachneCVTest | tail -n 1;
 bin/ArachneBlockSignalTest | tail -n 1;
-bin/ThreadTurnaround | tail -n 1 ) | scripts/column.py -s,
+bin/ThreadTurnaround | tail -n 1 ) 2>/dev/null | scripts/column.py -s,
 
 
 # Arbiter tests
@@ -20,6 +20,6 @@ if [[ "$1" == "--runArbiterBenchmarks" ]]; then
 echo
 (echo Benchmark,Count,Avg,Median,Min,99%,99.9%,99.99%,Max ;
 bin/CoreRequest_Contended_Latency | tail -n 1;
-bin/CoreRequest_Noncontended_Latency | tail -n 1) | scripts/column.py -s,
+bin/CoreRequest_Noncontended_Latency | tail -n 1) 2>/dev/null | scripts/column.py -s,
 fi
 # bin/CoreRequest_Contended_Timeout_Latency | tail -n 1;
