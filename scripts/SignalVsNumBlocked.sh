f7c336021347ea9c1fb2b7888b6fc8e38eee87a6
Henry Qin <hq6@cs.stanford.edu>
2018-04-25 15:22:14 -0600

    Omit the first run to remove bias from Core Arbiter setup

--- scripts/SignalVsNumBlocked.sh
+++ scripts/SignalVsNumBlocked.sh
@@ -4,6 +4,9 @@
 dirPATH=$(dirname $(dirname $(readlink -f $0)))
 cd ${dirPATH}
 
+# Do one run to warm up the core arbiter
+bin/ArachneBlockSignalTest 2> /dev/null >&2
+
 echo "Occupied Thread Slots,Median Latency,99% Latency"
 for i in $(seq 0 55); do
     echo -n $i,
