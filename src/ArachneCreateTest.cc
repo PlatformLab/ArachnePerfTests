d28d475ce46ec2bb0ac2fd7281a038f4fb569b7e
Henry Qin <hq6@stanford.edu>
2018-05-10 11:37:36 -0700

    Add conditionally compiled TimeTrace to ArachneCreateTest

--- src/ArachneCreateTest.cc
+++ src/ArachneCreateTest.cc
@@ -4,6 +4,7 @@
 #include <vector>
 
 #include "Arachne/Arachne.h"
+#include "PerfUtils/TimeTrace.h"
 #include "PerfUtils/Cycles.h"
 #include "PerfUtils/Stats.h"
 #include "PerfUtils/Util.h"
@@ -11,17 +12,39 @@
 #define NUM_SAMPLES 10000000
 #define MEAN_DELAY 0.000002
 
+using PerfUtils::Cycles;
+using PerfUtils::TimeTrace;
+
 namespace Arachne {
 extern bool disableLoadEstimation;
 }
 
-using PerfUtils::Cycles;
+// Uncomment the following line to enable TimeTraces.
+// #define TIME_TRACE 1
+
+// Provides a cleaner way of invoking TimeTrace::record, with the code
+// conditionally compiled in or out by the TIME_TRACE #ifdef. Arguments
+// are made uint64_t (as opposed to uin32_t) so the caller doesn't have to
+// frequently cast their 64-bit arguments into uint32_t explicitly: we will
+// help perform the casting internally.
+static inline void
+timeTrace(const char* format,
+        uint64_t arg0 = 0, uint64_t arg1 = 0, uint64_t arg2 = 0,
+        uint64_t arg3 = 0)
+{
+#if TIME_TRACE
+    TimeTrace::record(format, uint32_t(arg0), uint32_t(arg1),
+            uint32_t(arg2), uint32_t(arg3));
+#endif
+}
+
 
 uint64_t latencies[NUM_SAMPLES];
 static uint64_t arrayIndex = 0;
 
 void
 task(uint64_t creationTime) {
+    timeTrace("Creation completed");
     uint64_t startTime = Cycles::rdtsc();
     PerfUtils::Util::serialize();
     uint64_t latency = startTime - creationTime;
@@ -51,6 +74,7 @@ realMain() {
             ;
         PerfUtils::Util::serialize();
         uint64_t creationTime = Cycles::rdtsc();
+        timeTrace("About to create");
         Arachne::ThreadId id =
             Arachne::createThreadOnCore(core1, task, creationTime);
         Arachne::join(id);
@@ -94,5 +118,9 @@ main(int argc, const char** argv) {
         latencies[i] = Cycles::toNanoseconds(latencies[i]);
     printStatistics("Thread Creation Latency (NLB)", latencies, NUM_SAMPLES,
                     "data");
+#if TIME_TRACE
+        TimeTrace::setOutputFileName("ArachneCreateTest_TimeTrace.log");
+        TimeTrace::print();
+#endif
     return 0;
 }
