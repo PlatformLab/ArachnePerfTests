31405234c7cf42f900cb34bc41f7daf9cd09aa9b
Henry Qin <hq6@stanford.edu>
2018-09-25 15:09:16 -0700

    Fix SignalVsNumBlocked script to pass the number of sleeping threas correctly

--- scripts/SignalVsNumBlocked.sh
+++ scripts/SignalVsNumBlocked.sh
@@ -10,5 +10,5 @@ bin/ArachneBlockSignalTest 2> /dev/null >&2
 echo "Occupied Thread Slots,Median Latency,99% Latency"
 for i in $(seq 0 55); do
     echo -n $i,
-    bin/ArachneBlockSignalTest $i| tail -n 2 | awk -F, 'NR==2{print $4 "," $6}'
+    bin/ArachneBlockSignalTest -s $i| tail -n 2 | awk -F, 'NR==2{print $4 "," $6}'
 done
