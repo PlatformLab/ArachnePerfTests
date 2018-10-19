3735a7e93f6c5ce3ad58a1d46ef3687b78741e42
Henry Qin <hq6@cs.stanford.edu>
2018-10-19 14:26:11 -0600

    Revert "Change headers in script to include stddev"
    This reverts commit 8ebd283beec910d133add6f8a148eca4186eb327.
    Until we fix the master branch of PerfUtils to compute stats correctly, we
    should use the old headers.

--- scripts/RunAllTests.sh
+++ scripts/RunAllTests.sh
@@ -6,7 +6,7 @@ cd ${dirPATH}
 make
 
 # Threading primitive tests
-(echo Benchmark,Count,Avg,StdDev,Median,Min,99%,99.9%,99.99%,Max ;
+(echo Benchmark,Count,Avg,Median,Min,99%,99.9%,99.99%,Max ;
 bin/ArachneCreateTest $HTACTIVE | tail -n 1 ;
 bin/NullYieldTest  $HTACTIVE | tail -n 1 ;
 bin/ArachneYieldTest $HTACTIVE | tail -n 1 ;
@@ -18,7 +18,7 @@ bin/ArachneBlockSignalTest $HTACTIVE | tail -n 1 ) 2>/dev/null | scripts/column.
 # Arbiter tests
 if [[ "$1" == "--runArbiterBenchmarks" ]]; then
 echo
-(echo Benchmark,Count,Avg,StdDev,Median,Min,99%,99.9%,99.99%,Max ;
+(echo Benchmark,Count,Avg,Median,Min,99%,99.9%,99.99%,Max ;
 bin/CoreRequest_Contended_Latency | tail -n 1;
 bin/CoreRequest_Noncontended_Latency | tail -n 1) 2>/dev/null | scripts/column.py -s,
 fi
